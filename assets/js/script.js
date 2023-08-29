b70eaf80aba125aecf36a2a55d4e7e6ba7435bca
Gabriel <gabriel.gracilianomb@gmail.com>
2023-08-29 17:04:40 -0300

    Improved rating buttons and page transitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $(".component__submit").click(function() {
     $(".component__rating").css("display", "none");
-    $(".component__final").css("display", "block");
+    $(".component__final").fadeIn(600).css("display", "block");
+  });
+
+  $(".rating__btn").click(function() {
+    $(".rating__btn").removeClass("rating__btn--selected");
+    $(this).addClass("rating__btn--selected");
   });
 });
