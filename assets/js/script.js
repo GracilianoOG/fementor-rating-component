3fd590a15bd16eb5be37bba403ca0e05a3b4b27f
Gabriel <gabriel.gracilianomb@gmail.com>
2023-08-30 21:26:37 -0300

    Finished Transition Logic - The submit button will not work unless a rating button is pressed - Organized the code to avoid multiple DOM accesses - Improved readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-  $(".component__submit").click(function() {
+  const btnSubmit = $(".component__submit");
+  const ratingWrapper = $(".rating");
+  const ratingBtns = $(".rating__btn");
+
+  btnSubmit.attr("disabled", true);
+  btnSubmit.click(function() {
     const rating = $(".rating__btn--selected").text();
     $(".component__text span").text(rating);
     $(".component__rating").css("display", "none");
     $(".component__final").fadeIn(600).css("display", "block");
   });
 
-  $(".rating").on("click", ".rating__btn", function() {
-    $(".rating__btn").removeClass("rating__btn--selected");
+  ratingWrapper.on("click", ".rating__btn", function() {
+    if(!ratingWrapper.data("clicked")) {
+      ratingWrapper.data("clicked", true);
+      btnSubmit.removeClass("component__submit--disabled");
+      btnSubmit.attr("disabled", false);
+    }
+    ratingBtns.removeClass("rating__btn--selected");
     $(this).addClass("rating__btn--selected");
   });
 });
